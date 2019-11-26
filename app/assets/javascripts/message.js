2d70bad840ec458a072f8a294d937e027498231e
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-11-27 00:27:37 +0900

    カスタムデータ属性の追加とAPIコントローラの作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,6 @@
 $(function() {
+  
+ 
   function buildMessage(message) {
       var img = message.image ? `<img src= ${message.image}>` : "";
       var html = `<div class="main__messages__box">
@@ -45,4 +47,25 @@ $(function() {
       alert('error00');
     });
   })
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data('message-id');
+    url = 
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: ※※※,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
+
